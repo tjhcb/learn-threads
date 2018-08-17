3e8ca6908b6970fbb70b7d43f0a0801c05ce38be
tjhcb <tejaswinibelekar.2@gmail.com>
2018-08-17 15:35:55 +0530

    producer/consumer examples using sync list and BlockingQueue

---
+++ src/com/practice/producer/consumer/ex/Message.java
@@ -0,0 +1,14 @@
+package com.practice.producer.consumer.ex;
+
+public class Message {
+	
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
